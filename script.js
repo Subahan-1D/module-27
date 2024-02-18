718dd8b2a577fff8e3afb31ba8d6db6f5e27b37a
Subahan-1D <subahanislam523@gmail.com>
2024-02-19 02:35:18 +0600

    next tomorow

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 
+// function play(){
+//   const homeSection = document.getElementById('home-screen')
+//   homeSection.classList.add('hidden')
+// //   console.log(homeSection.classList.add('hidden'))
+
+// const playGroundSection= document.getElementById('play-ground')
+// playGroundSection.classList.remove('hidden')
+// // console.log(playGroundSection.classList)
+// }
+
+function continueGame(){
+
+    const alphabet = getARandomAlphabets();
+    console.log('Your Random Alphabet : ',alphabet)
+
+    // Alphabet generate randomly show it
+
+    const currentAlphabetGenerate = document.getElementById('current-alphabet')
+    currentAlphabetGenerate.innerText = alphabet ;
+
+    // set background color 
+
+    setBackgroundColorById(alphabet);
+
+}
+
 function play(){
-    console.log("Play Start")
+    hideElementById('home-screen')
+    showElementById('play-ground')
+    continueGame()
 }
---
+++ utility.js
@@ -0,0 +1,35 @@
+function hideElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden');
+}
+
+function showElementById(elementId) {
+    const element = document.getElementById(elementId)
+    element.classList.remove('hidden')
+}
+
+// set background
+
+function  setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId)
+    element.classList.add('bg-orange-400');
+
+}
+function getARandomAlphabets() {
+    // get or create or alphabet array
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz'
+    const alphabets = alphabetString.split('')
+    // console.log(alphabets);
+
+    // get a random access between 0-25 
+    const randomNumber = Math.random() * 25;
+    const index = Math.round(randomNumber);
+    // console.log(index);
+
+    const alphabet = alphabets [index]
+    console.log(index,alphabet)
+    return alphabet;
+
+
+}
+
